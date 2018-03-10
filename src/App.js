7822acbd04be92ebab05b17fe6cd6b0080fc2f82
= <jammydav93@gmail.com>
2018-03-10 20:45:25 +0000

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import React, { Component } from "react";
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Homepage from "./Homepage";
+import Home from "./Home";
+import { login, logout, isLoggedIn } from './utils/AuthService';
+import './index.css';
 
-class Main extends Component {
+
+class App extends Component {
   render() {
       return (
         <HashRouter>
           <div>
-            <h1>Simple SPA</h1>
-            <ul className="header">
-              <li><a href="/">Home</a></li>
-              <li><a href="/stuff">Stuff</a></li>
-              <li><a href="/contact">Contact</a></li>
-            </ul>
-            <div className="content">
+            <h1>Finance Forecaster SPA</h1>
 
+            <ul className="header">
+              <li><NavLink exact to="/">Home</NavLink></li>
+              <li><NavLink to="/homepage">Homepage</NavLink></li>
+              <li><a href="http://localhost:3000/login">Login</a></li>
+              <li>
+               {
+                 (isLoggedIn()) ? ( <button className="btn btn-danger log" onClick={() => logout()}>Log out </button> ) : ( <button className="btn btn-info log" onClick={() => login()}>Log In</button> )
+               }
+              </li>
+            </ul>
+
+            <div className="content">
+              <Route exact path="/" component={Home}/>
+              <Route path="/homepage" component={Homepage}/>
             </div>
+
+
           </div>
         </HashRouter>
       );
     }
 }
 
-export default Main;
+export default App;
