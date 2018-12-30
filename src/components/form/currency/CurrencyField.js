bdc36177066b00fb0698ae7d6e2868d183241f66
jrdavenport <jammydav93@gmail.com>
2018-12-30 20:49:36 +0000

    Replace react-currency-field with custom component

--- src/components/form/currency/CurrencyField.js
+++ src/components/form/currency/CurrencyField.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import { Field } from 'redux-form';
-import CurrencyInput from 'react-currency-input';
+
+const isAllowedValue = (value) => 
+  !isNaN(value) 
+  || value === '.'
+  || value === ','
+
+const CurrencyInput = ({ input }) => 
+  <input
+    onChange={input.onChange}
+    onBlur={input.onChange}
+    onKeyPress={(event) => {
+      const keyCode = event.keyCode || event.which;
+      const keyValue = String.fromCharCode(keyCode);
+      if (!isAllowedValue(keyValue)){
+        return event.preventDefault();
+      }
+    }} 
+  />
 
 const CurrencyField = ({ name }) =>
   <Field
     name={name}
-    component={props =>
-      <CurrencyInput 
-        value={ props.input.value }
-        onBlur={props.input.onChange}
-      />
-    }
+    component={CurrencyInput}
   />
 
 export default CurrencyField;
-
-            
