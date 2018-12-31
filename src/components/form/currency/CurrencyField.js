b714416e5ee7315b261cf94996b1603f9811d30d
jrdavenport <jammydav93@gmail.com>
2018-12-31 12:51:55 +0000

    Normalise currency inputs

--- src/components/form/currency/CurrencyField.js
+++ src/components/form/currency/CurrencyField.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import { Field } from 'redux-form';
 
+const normalize = (value, prevValue) => {
+  if (!value){
+    return '0.00'
+  }
+
+  const parsedValue = parseFloat(value)
+
+  if (isNaN(parsedValue)){
+    console.log('return 000')
+    return '0.00'
+  }
+
+  const decimalRegex = /(\d*)(\.?)(\d?\d?)/g
+  const matches = decimalRegex.exec(value)
+  const normalisedValue = matches[0] || '' + matches[1] || '' + matches[2] || ''
+
+  return normalisedValue
+
+}
+
 const CurrencyInput = ({ input }) => 
   <input
     type="number"
@@ -13,6 +33,7 @@ const CurrencyField = ({ name }) =>
   <Field
     name={name}
     component={CurrencyInput}
+    normalize={normalize}
   />
 
 export default CurrencyField;
