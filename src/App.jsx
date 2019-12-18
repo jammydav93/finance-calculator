5cfb629dda0cfa62a5dd16eb131fcaa899c7b542
jrdavenport <jammydav93@gmail.com>
2019-12-18 16:45:03 +0000

    trst ommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,10 @@
 import React from 'react';
-import {
-  Switch,
-  Route,
-} from 'react-router-dom';
-import Navigation from './components/Navigation';
-import Projector from './components/Projector';
 import styles from './App.module.scss';
 import WithAuthentication from './components/WithAuthentication';
 
 export const App = () => (
   <div className={styles.app}>
-    <div className={styles.header}>
-      <Navigation />
-    </div>
-    <div className={styles.content}>
-      <Switch>
-        <Route path="/version">
-          <div>{process.env.REACT_APP_VERSION}</div>
-        </Route>
-        <Route path="/">
-          <Projector />
-        </Route>
-      </Switch>
-    </div>
+    This is a testy test
   </div>
 );
 
